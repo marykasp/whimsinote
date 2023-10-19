284670d1991674e0a8013c785524d90ec3dd0147
marykasp <kasparian.mary@gmail.com>
2023-10-19 10:06:00 -0500

    chore: folder setup with routes and organize folders with () which are not part of the route url

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-red-500 text-white h-full">{children}</div>;
+};
+
+export default RootLayout;
