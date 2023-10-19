3b704544672a48223169416e32af6e8ab07b0620
marykasp <kasparian.mary@gmail.com>
2023-10-19 11:20:16 -0500

    style: add navbar and mode toggler for light and dark modes
    Change the background color for dark mode

--- app/(marketing)/_components/Heroes.tsx
+++ app/(marketing)/_components/Heroes.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Heroes = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5xl">
-      <div className="flex items-center">
+      <div className="flex items-center gap-x-7">
         <div className="relative w-[300px] h-[300px] sm:w-[350xp] sm:h-[350px] md:h-[400px] md:w-[400px]">
           <Image
             src="/documents.svg"
